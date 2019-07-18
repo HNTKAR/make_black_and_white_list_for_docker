3c0e402eac665e0a6b7c31430c4fb7c82e29a3a7
kusari-k <s15426@salesio-sp.ac.jp>
2019-07-18 23:41:15 +0900

    backup log file

--- diff-file.sh
+++ diff-file.sh
@@ -16,6 +16,8 @@ cat delegated-apnic-latest | grep JP |grep ipv4 >tmp
 sed -i -e "s*|256*/24*" -e "s*|512*/23*" -e "s*|1024*/22*" -e "s*|2048*/21*" -e "s*|4096*/20*" -e "s*|8192*/19*" -e "s*|16384*/18*" -e "s*|32768*/17*" -e "s*|65536*/16*"  -e "s*|131072*/15*" -e "s*|262144*/14*" -e "s*|524288*/13*" -e "s*|1048576*/12*" -e "s*|2097152*/11*" -e "s*|4194304*/10*" -e "s*|8388608*/9*" -e "s*|16777216*/8*" tmp
 cut -f 4 -d "|" tmp > japan-list
 echo 192.168.0.0/16 >> japan-list
+echo 172.16.0.0/12 >> japan-list
+echo 10.0.0.0/8 >> japan-list
 
 rm tmp delegated-apnic-latest
 
@@ -33,7 +35,11 @@ cat japan-list | xargs -I {} ipset add white-list "{}"
 chmod 777 black_list_log
 cat $mail_log_file | grep  SASL|sed  -e 's/.*\[//g' -e 's/\].*'//g >>black_list_log
 cat black_list_log | awk  'a[$0]++ == 10'>enemy_list
+
+cat $mail_log_file >>$mail_log_file`date +"%y%m%d"`
+rm -f $mail_log_file`date +"%y%m%d" -d "30 days ago"`
+
 `:`> $mail_log_file
 
 ipset flush black-list
-cat enemy_list | xargs -I {} ipset add black_list "{}"
+cat enemy_list | xargs -I {} ipset add black-list "{}"
